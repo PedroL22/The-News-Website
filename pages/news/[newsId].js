0a58f7ca8e2f6cf4335f67827f5ae739088c3dc0
Pedro L <pedrocontact22@gmail.com>
2022-09-27 18:11:43 -0300

    Added routing to each individual news

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,19 +1,46 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import NavBar from "../../components/NavBar";
 
-export default function News() {
-	const router = useRouter();
+export async function getStaticProps(context) {
+	const { params } = context;
 
-	const newsId = router.query.newsId;
+	const data = await fetch(
+		`https://content.guardianapis.com/search?api-key=4c444140-8216-4eb9-a8a0-5392b4af62ae&show-blocks=all${params.newsId}`
+	);
 
+	const newsData = await data.json();
+
+	const news = await newsData.response?.results;
+
+	return {
+		props: { ...news },
+	};
+}
+
+export async function getStaticPaths() {
+	const res = await fetch(
+		"https://content.guardianapis.com/search?api-key=4c444140-8216-4eb9-a8a0-5392b4af62ae&show-blocks=all"
+	);
+
+	const dataResponse = await res.json();
+
+	const data = await dataResponse.response.results;
+
+	const paths = data.map((news) => {
+		return {
+			params: { newsId: `${news?.blocks.main.id}` },
+		};
+	});
+
+	return { paths, fallback: true };
+}
+
+export default function News({ news }) {
 	return (
 		<>
-			<NavBar />
 			<Link href="/">
 				<a>Back</a>
 			</Link>
-			<h1>Exibindo o: {newsId}</h1>
+			<h1>Exibindo o: {news?.blocks.main.id}</h1>
 			<p>Comments: </p>
 		</>
 	);
