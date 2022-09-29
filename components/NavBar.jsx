0e0b816c7bf00929db5041c35cbe24acf266f984
Pedro L <pedrocontact22@gmail.com>
2022-09-29 01:27:42 -0300

    Added postgres database to Next Auth

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
-import React from 'react';
+import {signIn} from 'next-auth/react'
 
 export default function NavBar() {
   return (
     <nav className="fixed h-16 bg-red-700 mb-10 w-screen">
       <div className="flex mx-auto max-w-7xl justify-between p-5 w-screen">
         <Link href="/"><h1 className="text-white font-medium cursor-pointer">The News Website</h1></Link>
-				<Link href="#"><h1 className="text-white font-medium cursor-pointer">Login</h1></Link>
+				<button onClick={() => signIn()} className="text-white font-medium cursor-pointer">Login</button>
       </div>
     </nav>
   );
